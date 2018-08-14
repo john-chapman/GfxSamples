bbe1ae91315fd62aaa337b49627139c43d32ee03
John Chapman <j.x.chapman@gmail.com>
2018-08-14 10:44:12 +0200

    BC1 WIP

--- data/BlockCompression/shaders/BC1_cs.glsl
+++ data/BlockCompression/shaders/BC1_cs.glsl
@@ -1,8 +1,8 @@
 #include "shaders/def.glsl"
 
 #define USE_SHARED_MEMORY  1  // Load src block into shared memory (else call texelFetch() for each sample).
-#define ENDPOINTS_PCA      1  // Use principle component analysis to find the block endpoints (else use the color space extents).
-#define INDICES_EUCLIDEAN  1  // Use euclidean distance to find the indices per pixel (else project onto the line segment).
+#define ENDPOINTS_PCA      0  // Use principle component analysis to find the block endpoints (else use the color space extents).
+#define INDICES_EUCLIDEAN  0  // Use euclidean distance to find the indices per pixel (else project onto the line segment).
 
 uniform sampler2D txSrc;
 
@@ -15,12 +15,10 @@ layout(std430) restrict writeonly buffer _bfDst
 	shared vec3 s_srcTexels[16];
 	#define SRC_TEXEL(_i) s_srcTexels[_i]
 #else
-	#define SRC_TEXEL(_i)  texelFetch(txSrc, ivec2(gl_WorkGroupID.xy * 4) + ivec2(_i % 4, _i / 4), 0).rgb
+	#define SRC_TEXEL(_i) texelFetch(txSrc, ivec2(BLOCK_COORD) + ivec2(_i % 4, _i / 4), 0).rgb
 #endif
 
 shared uint s_indices[16];
-
-
 
 uint PackRGB565(in vec3 _rgb)
 {
@@ -51,17 +49,21 @@ float Covariance(in int _x, in int _y, in vec3 _avg)
 
 void main()
 {
-	#define THREAD_IDX gl_LocalInvocationIndex
+	#define TEXEL_INDEX (gl_LocalInvocationIndex)
+	#define TEXEL_COORD (gl_LocalInvocationID.xy)
+	#define BLOCK_INDEX (gl_WorkGroupID.y * gl_NumWorkGroups.x + gl_WorkGroupID.x)
+	#define BLOCK_COORD (gl_WorkGroupID.xy * 4)
 
 	#if USE_SHARED_MEMORY
 	{
 	 // load src block into shared memory (1 fetch per thread)
-		ivec2 iuv = ivec2(gl_WorkGroupID.xy * 4 + gl_LocalInvocationID.xy);
-		s_srcTexels[THREAD_IDX] = texelFetch(txSrc, iuv, 0).rgb;
+		ivec2 iuv = ivec2(BLOCK_COORD + TEXEL_COORD);
+		s_srcTexels[TEXEL_INDEX] = texelFetch(txSrc, iuv, 0).rgb;
 		groupMemoryBarrier();
 	}
 	#endif
 	#if 0
+	{
 	 // copy src block into registers
 		vec3 srcTexels[16];
 		for (int i = 0; i < 16; ++i) {
@@ -69,6 +71,7 @@ void main()
 		}
 		#undef SRC_TEXEL
 		#define SRC_TEXEL(i) srcTexels[i]
+	}
 	#endif
 
 	uvec2 block = uvec2(0);
@@ -159,7 +162,6 @@ void main()
 		palette[3] = 1.0/3.0 * palette[0] + 2.0/3.0 * palette[1];
 		#if 1
 		 // pack/unpack the palette values = quantize palette so that texel indices are generated from the final result
-		 // \todo it's not clear that this significantly improves the quality
 			for (int i = 0; i < 4; ++i) {
 				palette[i] = UnpackRGB565(PackRGB565(palette[i]));
 			}
@@ -168,38 +170,45 @@ void main()
 		int idx = 0;
 		float minErr = 999.0;
 		for (int i = 0; i < 4; ++i) {
-			float err = length2(SRC_TEXEL(THREAD_IDX) - palette[i]);
+			float err = length2(SRC_TEXEL(TEXEL_INDEX) - palette[i]);
 			if (err < minErr) {
 				minErr = err;
 				idx = i;
 			}
-			s_indices[THREAD_IDX] = idx;
+			s_indices[TEXEL_INDEX] = idx;
 		}
 	}
 	#else
 	{
+		#if 1
+		 // pack/unpack the endpoint values = quantize endpoints to minimize final error
+			ep0 = UnpackRGB565(PackRGB565(ep0));
+			ep1 = UnpackRGB565(PackRGB565(ep1));
+		#endif
+		
 	 // project onto (ep1 - ep0)
 		vec3 d = ep1 - ep0;
 		float dlen = length(d);
-		d /= dlen;
-	 	vec3 src = SRC_TEXEL(THREAD_IDX);
+		d /= dlen; // \todo fails for solid color blocks (dlen is 0)
+	 	vec3 src = SRC_TEXEL(TEXEL_INDEX);
 		float idx = dot(src - ep0, d) / dlen;
 
-	 // round to nearest palette index
+	 // round to nearest index
 		idx = round(saturate(idx) * 3.0);
 		const uvec4 idxMap = uvec4(1, 3, 2, 0);
-		s_indices[THREAD_IDX] = idxMap[uint(idx)];
+		s_indices[TEXEL_INDEX] = idxMap[uint(idx)];
 	}
 	#endif
+	groupMemoryBarrier();
 
  // encode block
-	if (THREAD_IDX == 0) {
+	if (TEXEL_INDEX == 0) {
 	 // pack palette indices
 		for (int i = 0; i < 16; ++i) {
-			block[1] = bitfieldInsert(block[1], s_indices[i], (i * 2), 2);
+			block[1] = bitfieldInsert(block[1], s_indices[i], i * 2, 2);
 		}	
 	 // write block data
-		bfDst[gl_WorkGroupID.y * gl_NumWorkGroups.x + gl_WorkGroupID.x] = block;
+		bfDst[BLOCK_INDEX] = block;
 	}
 	
 }
