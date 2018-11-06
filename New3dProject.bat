ca5758e074bebbcf27f5f46984e1bb4dbd01999f
John Chapman <j.x.chapman@gmail.com>
2018-11-06 15:33:23 +0100

    Fixed New*Project.bat

--- New3dProject.bat
+++ New3dProject.bat
@@ -3,7 +3,6 @@ set /p projName= Project name:
 
 call build\tools\lua53.exe build\new_project.lua %projName% "AppSample3d" || pause
 
-if %ERRORLEVEL% equ 0 
-(
+if %ERRORLEVEL% equ 0 (
 	call build\tools\premake5.exe --file=build\premake.lua vs2017 || pause
-)
+	)
